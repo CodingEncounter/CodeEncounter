0e513f6884265559d8d38dc629eb9aa7e40a8b35
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-07-13 12:40:42 -0500

    Update PictureRepo.java

--- src/main/java/com/codeup/codeencounter/repositories/PictureRepo.java
+++ src/main/java/com/codeup/codeencounter/repositories/PictureRepo.java
@@ -1,11 +1,16 @@
 package com.codeup.codeencounter.repositories;
 
+import com.codeup.codeencounter.models.Gallery;
+import com.codeup.codeencounter.models.Picture;
 import com.codeup.codeencounter.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface UserRepo extends JpaRepository <User, Long> {
-    User findByUsername(String username);
-    User findByEmail(String email);
+public interface PictureRepo extends JpaRepository <Picture, Long> {
+
+    Picture findById(long id);
+    List<Picture> findAllByUser(User user);
+    List<Picture> findAllByGallery(Gallery gallery);
+    Picture findByComment(String comment);
 }
