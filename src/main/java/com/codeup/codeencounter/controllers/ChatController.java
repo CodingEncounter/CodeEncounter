78d7d76bc09c40f668a62a391bfcbe4f1a5a968c
cortiz642 <chris.o.ortiz93@gmail.com>
2022-07-14 08:05:07 -0500

    updated chatcontroller and input additional maven dependencies

--- src/main/java/com/codeup/codeencounter/controllers/ChatController.java
+++ src/main/java/com/codeup/codeencounter/controllers/ChatController.java
@@ -1,4 +1,27 @@
 package com.codeup.codeencounter.controllers;
 
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ChatController {
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/javainuse")
+    public WebSocketChatMessage sendMessage(@Payload WebSocketChatMessage webSocketChatMessage) {
+        return webSocketChatMessage;
+    }
+
+    @MessageMapping("/chat.newUser")
+    @SendTo("/topic/javainuse")
+    public WebSocketChatMessage newUser(@Payload WebSocketChatMessage webSocketChatMessage,
+                                        SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", webSocketChatMessage.getSender());
+        return webSocketChatMessage;
+    }
+
 }
+
