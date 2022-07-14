ac3e21c335ce3aa6a70c368579dfc010f8005633
cortiz642 <chris.o.ortiz93@gmail.com>
2022-07-14 11:40:46 -0500

    troubleshooting UserChatController issue

--- src/main/java/com/codeup/codeencounter/controllers/UserChatController.java
+++ src/main/java/com/codeup/codeencounter/controllers/UserChatController.java
@@ -1,4 +1,27 @@
 package com.codeup.codeencounter.controllers;
 
+
+import com.codeup.codeencounter.models.UserChat;
+import com.codeup.codeencounter.repositories.UserChatRepo;
+import org.apache.catalina.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("*")
+@RestController
 public class UserChatController {
+    @Autowired
+    UserChatRepo.UserRepository userRepository;
+
+    @PostMapping(value = "/createUser")
+    public ResponseEntity<UserChat> createUserChat(@RequestBody User user){
+       return new ResponseEntity<>(userRepository.save(user), HttpStatus.OK);
+    }
+    @GetMapping(value = "/getUser")
+    public ResponseEntity<UserChat> getUser(@RequestParam(required = true) Long userId){
+        return new ResponseEntity<>(userRepository.findById(userId).get(), HttpStatus.OK);
+    }
 }
