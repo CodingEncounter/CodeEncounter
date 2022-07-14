ef8811d21e1887e8132d4ec9cf3889bdca38fbfd
cory-tidwell <101146141+cory-tidwell@users.noreply.github.com>
2022-07-14 14:48:46 -0400

    photo-upload-working

--- src/main/resources/static/js/filestack.js
+++ src/main/resources/static/js/filestack.js
@@ -1,19 +1,26 @@
 const fileStack = 'AX1Jt8rFiRvGNuWPENMkoz';
 const client = filestack.init(fileStack);
 
+
 const options = {
     fromSources: ["local_file_system"],
     accept: ["image/*"],
     onFileUploadFinished: callback => {
         const filestackUrl = callback.url;
+        setTimeout(() => {
         $('#photo').val(filestackUrl);
         alert("Upload Success!")
+        },5000);
     }
 };
-$('#upload').click(function (event){
-    event.preventDefault();
-    client.picker(options).open();
-})
+
+
+
+    $('#upload').click(function (event){
+        event.preventDefault();
+        client.picker(options).open();
+    })
+
 
 function validatePhoto(){
     $(document.getElementById($('submitAddPhoto'))).css('display: block;');
