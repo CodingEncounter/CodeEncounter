bcf0ba618a9481668fecfb10d1574b94c6f9513e
cortiz642 <chris.o.ortiz93@gmail.com>
2022-07-18 23:30:42 -0500

    troubleshooting chat renderer not displaying

--- src/main/webapp/js/UserChat.js
+++ src/main/webapp/js/UserChat.js
@@ -1,29 +1,48 @@
-// Talk.ready.then(function () {
-//     var me = new Talk.User({
-//         id: '1',
-//         name: 'chris123',
-//         email: 'chris@gmail.com',
-//         welcomeMessage: 'Hey there! How are you? :-)',
-//     });
-//     window.talkSession = new Talk.Session({
-//         appId: '1',
-//         me: me,
-//     });
-//     var other = new Talk.User({
-//         id: '3',
-//         name: 'IronMaidenFangirl',
-//         email: 'TestEmail2@Test.com',
-//         welcomeMessage: 'Hey, how can I help?',
-//     });
-//
-//     var conversation = talkSession.getOrCreateConversation(
-//         Talk.oneOnOneId(me, other)
-//     );
-//     conversation.setParticipant(me);
-//     conversation.setParticipant(other);
-//
-//     // var conversation = talkSession.getOrCreateConversation('order_83562938');
-//
-//     var inbox = talkSession.createInbox({ selected: conversation });
-//     inbox.mount(document.getElementById('talkjs-container'));
-// });
+import Talk from 'talkjs';
+
+const getAgent = async () => {
+    const response = await fetch('http://localhost:8081/getUser?userId=1');
+    const data = await response.json();
+    let agent = new Talk.User({
+        id: data.id,
+        name: data.name,
+        photoUrl: data.dp,
+        email: data.email,
+        role: data.role
+    });
+    return agent;
+}
+const getUser = async () => {
+    const response = await fetch('http://localhost:8081/getUser?userId=2');
+    const data = await response.json();
+    let user = new Talk.User({
+        id: data.id,
+        name: data.name,
+        photoUrl: data.dp,
+        email: data.email,
+        role: data.role
+    });
+    return user;
+}
+(
+    async function() {
+        await Talk.ready;
+        let agent = await getAgent();
+        let user = await getUser();
+
+        const session = new Talk.Session({
+            appId: 'tOGmRmb6',
+            me: user,
+        });
+        var conversation = session.getOrCreateConversation(Talk.oneOnOneId(user, agent));
+
+        conversation.setAttributes({
+            welcomeMessages: ["You can start typing your message here and one of our agents will be with you shortly.", "Please do not divulge any of your personal information."]
+        })
+        conversation.setParticipant(user);
+        conversation.setParticipant(agent);
+
+        var chatbox = window.talkSession.createChatbox();
+        chatbox.select(conversation);
+        chatbox.mount(document.getElementById('talkjs-container'));
+    }());
