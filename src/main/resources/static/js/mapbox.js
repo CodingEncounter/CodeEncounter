85f051e12744ee117898e66d29c9c0d954948cea
Boryana1 <boryana.dimova1@gmail.com>
2022-07-26 06:03:25 -0500

    Cleaned the code.

--- src/main/resources/static/js/mapbox.js
+++ src/main/resources/static/js/mapbox.js
@@ -16,6 +16,7 @@ function errorLocation(){
     setupMap([longitude, latitude]);
 }
 
+// ADD A MAP
 function setupMap(center) {
     let map = new mapboxgl.Map({
         container: 'map',
@@ -23,6 +24,33 @@ function setupMap(center) {
         center: center,
         zoom: 14
     });
+    // function onMapLoaded(event) {
+    //
+    //     event.map.resize();
+    //
+    // }
+
+    //Search bar
+    const geocoder = new MapboxGeocoder({
+        // Initialize the geocoder
+        accessToken: mapboxgl.accessToken, // Set the access token
+        mapboxgl: mapboxgl, // Set the mapbox-gl instance
+        marker: false, // Do not use the default marker style
+        placeholder: '   Search for places', // Placeholder text for the search bar
+    });
+    function geocode(search, token) {
+        var baseUrl = 'https://api.mapbox.com';
+        var endPoint = '/geocoding/v5/mapbox.places/';
+        return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+            .then(function(res) {
+                return res.json();
+                // to get all the data from the request, comment out the following three lines...
+            }).then(function(data) {
+                return data.features[0].center;
+            });
+    }
+
+
     // const nav = new mapboxgl.NavigationControl();
     map.addControl(new mapboxgl.NavigationControl())
     // nav.addTo(map);
@@ -31,19 +59,34 @@ function setupMap(center) {
         unit: 'metric',
         profile: 'mapbox/cycling'
     });
-
-    setTimeout(function(){
         map.addControl(directions, 'top-left');
-        },1000);
-//    create a marker on double click
+
+    var marker = new mapboxgl.Marker({
+        draggable: true
+    })
+        .setLngLat([longitude, latitude])
+        .addTo(map);
+
+    //    create a marker on double click
+    // function add_marker (event) {
+    //     var coordinates = event.lngLat;
+    //     console.log('Lng:', coordinates.lng, 'Lat:', coordinates.lat);
+    //     marker.setLngLat(coordinates).addTo(map);
+    // }
+    //
+    // map.on('dblclick', add_marker);
 
     var marker = new mapboxgl.Marker()
 
-    function add_marker (event) {
-        var coordinates = event.lngLat;
-        console.log('Lng:', coordinates.lng, 'Lat:', coordinates.lat);
-        marker.setLngLat(coordinates).addTo(map);
+// Adding functionality to draggable marker
+    function onDragEnd() {
+        var lngLat = marker.getLngLat();
+        longitude = lngLat.lng;
+        latitude = lngLat.lat;
     }
+    marker.on('dragend', onDragEnd);
 
-    map.on('dblclick', add_marker);
+
+    // Add the geocoder to the map
+    map.addControl(geocoder);
 }
