825af1705b52de0d18f2fd146d8785e26fd69fa4
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-08-01 15:06:30 -0500

    styling

---
+++ src/main/java/com/codeup/codeencounter/models/Meetup.java
@@ -0,0 +1,92 @@
+package com.codeup.codeencounter.models;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "meetups")
+public class Meetup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateTime;
+
+    @Column(nullable = false)
+    private String dateTimeString;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String longitude;
+
+    @Column(nullable = false)
+    private String latitude;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getDateTimeString() {
+        return dateTimeString;
+    }
+
+    public void setDateTimeString(String dateTimeString) {
+        this.dateTimeString = dateTimeString;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
