a51120fa2a8068da69291e85be399dccfc2e00f9
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-08-01 15:07:39 -0500

    styling

---
+++ src/main/java/com/codeup/codeencounter/repositories/ChatRepo.java
@@ -0,0 +1,14 @@
+//package com.codeup.codeencounter.repositories;
+//
+//
+//
+//import com.codeup.codeencounter.models.ChatMessage;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface UserChatRepo extends JpaRepository<ChatMessage, Long> {
+//
+//@Repository
+//public interface ChatRepo extends JpaRepository<ChatUser, Long>  {
+//}
