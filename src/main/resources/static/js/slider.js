eb81522709c87b9773a31f234aee8f8da63d8885
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-08-01 15:05:42 -0500

    styling

--- src/main/resources/static/js/slider.js
+++ src/main/resources/static/js/slider.js
@@ -5,29 +5,29 @@ let index = 0;
 console.log(slides);
 
 function display(index) {
-    slides.forEach((slide) => {
-        slide.style.display = "none";
-    });
-    slides[index].style.display = "flex";
-    // clearTimeout(timer);
-    // timer = setTimeout(() => slides(1), 2000);
+  slides.forEach((slide) => {
+    slide.style.display = "none";
+  });
+  slides[index].style.display = "flex";
+  // clearTimeout(timer);
+  // timer = setTimeout(() => slides(1), 2000);
 }
 
 function nextSlide() {
-    index++;
-    if (index > slides.length - 1) {
-        index = 0;
-    }
-    display(index);
-    setTimeout(nextSlide, 5000);
+  index++;
+  if (index > slides.length - 1) {
+    index = 0;
+  }
+  display(index);
+  setTimeout(nextSlide, 4000);
 }
 
 function prevSlide() {
-    index--;
-    if (index < 0) {
-        index = slides.length - 1;
-    }
-    display(index);
+  index--;
+  if (index < 0) {
+    index = slides.length - 1;
+  }
+  display(index);
 }
 
 next.addEventListener("click", nextSlide);
